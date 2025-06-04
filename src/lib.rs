1f6557a3a2a72da12121b7ad5af6fcec539329f7
谢克 <1023052645@qq.com>
2025-06-04 20:38:53 +0800

    upgrade remixicon to v4.6.0 and egui to 0.31.1

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ pub const FONT: &[u8] = include_bytes!("../assets/remixicon.ttf");
 
 #[cfg(feature = "egui")]
 pub fn add_to_fonts(fonts: &mut egui::FontDefinitions) {
-    fonts.font_data.insert("remixicon".into(), font_data().into());
+    fonts
+        .font_data
+        .insert("remixicon".into(), font_data().into());
 
     if let Some(font_keys) = fonts.families.get_mut(&egui::FontFamily::Proportional) {
         font_keys.push("remixicon".into());
